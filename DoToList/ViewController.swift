323377a66cf18f1ff4d2eb4415d01d839daafdae
Evgenii Mikhailov <e.mikhailov@outlook.com>
2024-11-15 10:30:08 +0300

    Initial Commit

---
+++ DoToList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoToList
+//
+//  Created by Evgenii Mikhailov on 15.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
